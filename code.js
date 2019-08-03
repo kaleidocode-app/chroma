4265c549daa5b3a9e96bec80694c4346ee7b9e9e
Miguel Solorio <miguel.solorio@microsoft.com>
2019-08-02 23:59:37 -0700

    Only create styles if no styles exist for item

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 if (figma.currentPage.selection.length <= 0) {
     figma.closePlugin('Please select a Rectanle, Ellipse, or Polygon before running this plugin');
 }
+let ignoredCounter = 0;
 let ref = [];
 let selection = figma.currentPage.selection;
 selection.forEach(c => {
@@ -10,28 +11,38 @@ ref.sort((one, two) => (one > two ? -1 : 1));
 ref.forEach((layer) => {
     // make sure it's a vector
     if (layer.type === "RECTANGLE" || layer.type === "ELLIPSE" || layer.type === "POLYGON" || layer.type === "VECTOR") {
-        //creating the paint style
-        var newStyle = figma.createPaintStyle();
-        //naming the paint style with the layer name
-        newStyle.name = layer.name;
-        //assigning the color
-        newStyle.paints = [{
-                type: layer.fills[0].type,
-                color: {
-                    r: layer.fills[0].color.r,
-                    g: layer.fills[0].color.g,
-                    b: layer.fills[0].color.b
-                },
-                opacity: layer.fills[0].opacity
-            }];
-        //applying the style to the selected layer
-        layer.fillStyleId = newStyle.id;
-        // console log the output
-        console.log('🎉 Created style ' + layer.name);
+        if (!layer.fillStyleId) {
+            //creating the paint style
+            var newStyle = figma.createPaintStyle();
+            //naming the paint style with the layer name
+            newStyle.name = layer.name;
+            //assigning the color
+            newStyle.paints = [{
+                    type: layer.fills[0].type,
+                    color: {
+                        r: layer.fills[0].color.r,
+                        g: layer.fills[0].color.g,
+                        b: layer.fills[0].color.b
+                    },
+                    opacity: layer.fills[0].opacity
+                }];
+            //applying the style to the selected layer
+            layer.fillStyleId = newStyle.id;
+            // console log the output
+            console.log('🎉 Created style ' + layer.name);
+        }
+        else {
+            ignoredCounter++;
+        }
     }
     else {
         figma.closePlugin('Please select a Rectanle, Ellipse, or Polygon before running this plugin');
     }
 });
 figma.currentPage.selection = [];
-figma.closePlugin();
+if (ignoredCounter > 0) {
+    figma.closePlugin('⚠️ Warning: ' + ignoredCounter + ' color style(s) already exist and can\'t be added');
+}
+else {
+    figma.closePlugin();
+}
--- code.ts
+++ code.ts
@@ -2,7 +2,7 @@ if (figma.currentPage.selection.length <= 0){
   figma.closePlugin('Please select a Rectanle, Ellipse, or Polygon before running this plugin')
 }
 
-
+let ignoredCounter = 0
 let ref = []
 let selection = figma.currentPage.selection
 
@@ -12,32 +12,40 @@ selection.forEach(c => {
 ref.sort((one, two) => (one > two ? -1 : 1));
 
 ref.forEach((layer:any) => {
+
   
   // make sure it's a vector
   if (layer.type === "RECTANGLE" || layer.type === "ELLIPSE" || layer.type === "POLYGON" || layer.type === "VECTOR") {
+    
+    if(!layer.fillStyleId){
+      
+      //creating the paint style
+      var newStyle = figma.createPaintStyle()
 
-    //creating the paint style
-    var newStyle = figma.createPaintStyle()
+      //naming the paint style with the layer name
+      newStyle.name = layer.name
 
-    //naming the paint style with the layer name
-    newStyle.name = layer.name
+      //assigning the color
+      newStyle.paints = [{
+        type: layer.fills[0].type,
+        color: {
+          r: layer.fills[0].color.r,
+          g: layer.fills[0].color.g,
+          b: layer.fills[0].color.b
+        },
+        opacity: layer.fills[0].opacity
+      }]
 
-    //assigning the color
-    newStyle.paints = [{
-      type: layer.fills[0].type,
-      color: {
-        r: layer.fills[0].color.r,
-        g: layer.fills[0].color.g,
-        b: layer.fills[0].color.b
-      },
-      opacity: layer.fills[0].opacity
-    }]
+      //applying the style to the selected layer
+      layer.fillStyleId = newStyle.id
 
-    //applying the style to the selected layer
-    layer.fillStyleId = newStyle.id
+      // console log the output
+      console.log('🎉 Created style ' + layer.name)
 
-    // console log the output
-    console.log('🎉 Created style ' + layer.name)
+    } else {
+      ignoredCounter++
+    }
+    
 
   } else {
     figma.closePlugin('Please select a Rectanle, Ellipse, or Polygon before running this plugin')
@@ -45,4 +53,9 @@ ref.forEach((layer:any) => {
 });
 
 figma.currentPage.selection = []
-figma.closePlugin();
+
+if(ignoredCounter > 0){
+  figma.closePlugin('⚠️ Warning: ' + ignoredCounter + ' color style(s) already exist and can\'t be added')
+} else {
+  figma.closePlugin();
+}
